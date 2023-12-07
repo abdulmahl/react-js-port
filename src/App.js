6ef8c1b8f97c32436907b21691ccbb6dcce49eec
abdulmahl <mahalanguma87@gmail.com>
2023-12-07 23:18:44 +0200

    .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Homescreen/Index";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Home />
+  </div>;
 }
 
 export default App;
