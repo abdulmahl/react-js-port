b3556ee359f20a03b5ee8e509f3e34f1755fd9c5
abdulmahl <mahalanguma87@gmail.com>
2023-12-08 18:26:44 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./Pages/Home/Navbar";
 import Home from "./Pages/Homescreen/Index";
 
 function App() {
-  return <div className="App">
-    <Home />
-  </div>;
+  return (
+    <div className="App">
+      <Router>
+        <div>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="*" element={<div>404 Not Found</div>}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- src/Pages/Homescreen/Index.jsx
+++ src/Pages/Homescreen/Index.jsx
@@ -1,9 +1,11 @@
 import HeroSection from "../Home/HeroSection";
+import MySkills from "../Home/MySkills";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <MySkills />
     </>
   );
 };
