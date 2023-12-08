68decd9820ae0b05a1ccb878deeee0e116b0d445
abdulmahl <mahalanguma87@gmail.com>
2023-12-08 08:03:58 +0200

    .

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -2,22 +2,20 @@ const HeroSection = () => {
   return (
     <section id="heroSection" className="hero--section">
       <div className="hero--section--content--box">
-        <div className="hero--section-content">
+        <div className="hero--section--content">
           <p className="section--title">Hey, I'm Abdul...</p>
           <h1 className="hero--section--title">
-            <span className="hero--section-title--color"></span>{" "}
             Full Stack
-            <br />
+            <span className="hero--section--title--color"></span> <br />
             Developer
           </h1>
           <p className="hero--section-description">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             <br />
-            Deserunt,
-            nesciunt? Minus dignissimos eligendi doloremque.
+            Deserunt, nesciunt? Minus dignissimos eligendi doloremque.
           </p>
         </div>
-        <button className="btn btn-primary">Get in Touch</button>
+        <button type="submit" className="btn btn-primary">Get in Touch</button>
       </div>
       <div className="hero--section--image">
         <img src="./img/hero_img.png" alt="Hero-Image" />
