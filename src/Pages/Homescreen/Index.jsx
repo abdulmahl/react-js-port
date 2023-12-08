ab6efda6c3aaf80ce77a1fac100082aa21bbcc20
abdulmahl <mahalanguma87@gmail.com>
2023-12-08 20:48:59 +0200

    .

--- src/Pages/Homescreen/Index.jsx
+++ src/Pages/Homescreen/Index.jsx
@@ -2,6 +2,7 @@ import AboutMe from "../Home/AboutMe";
 import HeroSection from "../Home/HeroSection";
 import MyPortfolio from "../Home/MyPortfolio";
 import MySkills from "../Home/MySkills";
+import Testimonial from "../Home/Testimonials";
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <MySkills />
       <AboutMe />
       <MyPortfolio />
+      <Testimonial />
     </>
   );
 };
