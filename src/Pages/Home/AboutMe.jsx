091826640c65982596d7867f95ad08fb134f8596
abdulmahl <mahalanguma87@gmail.com>
2023-12-08 19:44:43 +0200

    .

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,5 +1,29 @@
 const AboutMe = () => {
-
-}
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img lazy-loading src="./img/about-me.png" alt="About-Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, fuga
+            perferendis. Commodi perspiciatis quis accusantium, vitae, expedita
+            nisi est incidunt autem odio sed quod, voluptatum enim voluptatem
+            cumque rem dolore.
+          </p>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, fuga
+            perferendis. Commodi perspiciatis quis accusantium, vitae, expedita
+            nisi est incidunt autem odio sed quod, voluptatum enim voluptatem
+            cumque rem dolore.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default AboutMe;
--- src/Pages/Homescreen/Index.jsx
+++ src/Pages/Homescreen/Index.jsx
@@ -1,4 +1,6 @@
+import AboutMe from "../Home/AboutMe";
 import HeroSection from "../Home/HeroSection";
+import MyPortfolio from "../Home/MyPortfolio";
 import MySkills from "../Home/MySkills";
 
 const Home = () => {
@@ -6,6 +8,8 @@ const Home = () => {
     <>
       <HeroSection />
       <MySkills />
+      <AboutMe />
+      <MyPortfolio />
     </>
   );
 };
