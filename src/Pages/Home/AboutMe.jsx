74a417057274135bdc7a2f7460f7b5ce9e9f49b2
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-06 11:08:16 +0200

    .

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -6,19 +6,21 @@ const AboutMe = () => {
       </div>
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
-          <p className="section--title">About</p>
           <h1 className="skills--section--heading">About Me</h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, fuga
-            perferendis. Commodi perspiciatis quis accusantium, vitae, expedita
-            nisi est incidunt autem odio sed quod, voluptatum enim voluptatem
-            cumque rem dolore.
+            Hello there my name is Abdul! I'm a second-year student at Brigham Young University,
+            pursuing a major in Applied Technology. Currently unemployed but
+            filled with ambition, I aspire to become a proficient full-stack
+            developer.
           </p>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet, fuga
-            perferendis. Commodi perspiciatis quis accusantium, vitae, expedita
-            nisi est incidunt autem odio sed quod, voluptatum enim voluptatem
-            cumque rem dolore.
+            My journey at BYU has equipped me with the skills and knowledge
+            necessary to tackle real-world challenges in the tech industry. I am
+            passionate about coding and problem-solving, and I'm eager to
+            contribute my skills to innovative projects. With a strong
+            foundation in applied technology and a drive to continuously learn
+            and grow, I'm ready to embark on an exciting career path in software
+            development.
           </p>
         </div>
       </div>
--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -5,27 +5,32 @@ const HeroSection = () => {
         <div className="hero--section--content">
           <p className="section--title">Hey, I'm Abdul...</p>
           <h1 className="hero--section--title">
-            Junior
+            Junior Front-end
             <span className="hero--section--title--color"></span> <br />
-            Developer
+            Web Developer
           </h1>
           <p className="hero--section-description">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            <br />
-            Deserunt, nesciunt? Minus dignissimos eligendi doloremque.
+            My ambition is to become a skilled full-stack developer. I'm
+            captivated by the intricate dance of front-end and back-end
+            development, and I'm driven to master the art of crafting seamless
+            digital experiences. With each line of code, I'm honing my skills
+            and expanding my knowledge, eager to contribute to cutting-edge
+            projects and make a meaningful impact in the ever-evolving world of
+            technology.
           </p>
         </div>
         <button type="submit" className="btn btn-primary">
           <a
             className="linked-in"
-            href="https://www.linkedin.com/in/abdul-mahlangu/" target="_blank"
+            href="https://www.linkedin.com/in/abdul-mahlangu/"
+            target="_blank"
           >
             Get in Touch
           </a>
         </button>
       </div>
       <div className="hero--section--image">
-        <img src="./img/hero-image.png" alt="Hero-Image" />
+        <img src="./img/image.jpg" alt="Hero-Image" />
       </div>
     </section>
   );
