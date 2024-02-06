97f02f9e7b1cf95bdf115cd8ef1ad619b1395d24
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-06 11:18:45 +0200

    .

--- src/Pages/Home/ContactMe.jsx
+++ src/Pages/Home/ContactMe.jsx
@@ -5,8 +5,7 @@ const ContactMe = () => {
         <p className="sub--title">Get In Touch</p>
         <h2>Contact Me</h2>
         <p className="text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
-          temporibus.
+          Please fill out this form to get in touch with me, I will respond to you in a timely manner.
         </p>
       </div>
       <form className="contact--form--container">
